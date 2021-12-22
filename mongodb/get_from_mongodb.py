869768d3164f779791d623ecfcc6e8ca0f8143f4
Felipe Demenech Vasconcelos <felipedmnq@gmail.com>
2021-12-22 09:11:04 +0100

    update get_from_mongodb.py

--- mongodb/get_from_mongodb.py
+++ mongodb/get_from_mongodb.py
@@ -10,12 +10,20 @@ from bson.json_util import dumps
 
 mongo_con = "mongodb://localhost:27017/"
 folder_to_save = '../data'
+db = 'openweather_db'
+collection = 'openweather'
 
-def openweather_mdb_to_json(mongo_con, folder_to_save):
+def openweather_mdb_to_json(mongo_con,
+                            db,
+                            collection,
+                            folder_to_save,):
+    '''Get data from MongoDB and saves as json
+    
+    '''
     # access to mongodb localy
     cl = pymongo.MongoClient(mongo_con)
     # get database.collection
-    ow_collection = cl.openweather_mdb.openweather
+    ow_collection = cl.db.collection
     # select all data
     data = ow_collection.find()
     # extract data to a list
@@ -30,16 +38,19 @@ def openweather_mdb_to_json(mongo_con, folder_to_save):
         os.makedirs(folder_to_save)
 
     today = datetime.today().date()
-    filename = f'openweather_{today}'.replace('-','')
+    filename = f'openweather_{today}.json'.replace('-','')
 
     # Writing data to file openweather.json
-    with open(f'{folder_to_save}/{filename}.json', 'w') as file:
+    with open(f'{folder_to_save}/{filename}', 'w') as file:
         file.write(json_data)
 
     print(f'{filename} saved at {folder_to_save}.')
 
 def main():
-    openweather_mdb_to_json(mongo_con=mongo_con,folder_to_save=folder_to_save)
+    openweather_mdb_to_json(mongo_con=mongo_con,
+                            folder_to_save=folder_to_save,
+                            db=db,
+                            collection=collection)
 
 if __name__=='__main__':
     main()
