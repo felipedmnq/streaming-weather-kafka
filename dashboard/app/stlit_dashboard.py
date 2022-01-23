2a01d91a0de04b1da41aa8f7ed1c790ba6796b89
Felipe Demenech Vasconcelos <felipedmnq@gmail.com>
2022-01-23 07:54:40 +0100

    playing a bit more with streamlit

--- dashboard/app/stlit_dashboard.py
+++ dashboard/app/stlit_dashboard.py
@@ -4,6 +4,8 @@ import streamlit as st
 from PIL import Image
 from datetime import datetime
 from pandas import DataFrame
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 st.set_page_config(layout="wide")
@@ -23,14 +25,33 @@ from get_mongodb.get_from_mongodb import openweather_mdb_to_json
 sys.path.insert(2, f"{MAIN_DIR}/pyspark")
 from jobs.pyspark_clean import *
 
+# UPDATE DATA
 def up_to_date_by_city(df: DataFrame) -> DataFrame:
     df1 = df.groupby(["city", "hour"])[["temp_F", "temp_C", "humidity"]].mean().reset_index()
     df1["temp_F"] = round(df1["temp_F"],2)
     df1["temp_C"] = round(df1["temp_C"],2)
     df1["humidity"] = round(df1["humidity"],2)
     hour = NOW.hour
-    df1 = df1[df1["hour"] == hour].drop(columns="hour").reset_index()
+    df1 = df1[df1["hour"] == hour].drop(columns="hour").reset_index(drop=True)
     return df1
+
+# GROUP CITIES BY DATE
+def groupby_date_city(df: DataFrame) -> DataFrame:
+    df["date"] = df["created_at"].apply(lambda date: date.strftime("%Y-%m-%d"))
+    df["date"] = pd.to_datetime(df["date"])
+    df = df.groupby(["date", "city"])[["temp_F", "temp_C", "humidity"]].mean().reset_index()
+    return df
+
+# LINEPLOT - TEMPERATURE BY DAY BY CITY
+def line_plot_by_day(df: DataFrame, city: str) -> None:
+    df3 = df[df["city"] == f"{city}"]
+    fig = plt.figure(figsize=(6, 2))
+    sns.lineplot(x=df3["date"], y=df3["temp_C"], markers='o')
+    plt.xticks(rotation=30)
+    plt.grid()
+    plt.title(f"Average Temerature by Day - {city}")
+    plt.ylabel("Temperature - C")
+    return fig
 
 def plot_temp(df):
     pass
@@ -51,71 +72,75 @@ with sidebar:
 
     st.sidebar.header("Get Data")
 
-    with get_data:
-        # walks cleansed folder and return the latest parquet file
-        latest_file = [*os.walk(f"{DATA_DIR}/cleansed")][0][1][-2]
-        file_path = f"{DATA_DIR}/cleansed/{latest_file}"
-        #spark = createSession(app_name="ST Prepare Data")
+with dashboard:
+    # walks cleansed folder and return the latest parquet file
+    latest_file = [*os.walk(f"{DATA_DIR}/cleansed")][0][1][-2]
+    file_path = f"{DATA_DIR}/cleansed/{latest_file}"
+    #spark = createSession(app_name="ST Prepare Data")
 
-        #data  = spark.read.parquet(file_path)
-        # CONVERT TO PANDAS
-        #data = data.toPandas()
-        #st.write(type(data))
-        #st.write(data.head())
+    #data  = spark.read.parquet(file_path)
+    # CONVERT TO PANDAS
+    #data = data.toPandas()
+    #st.write(type(data))
+    #st.write(data.head())
 
-        last_update = f"LAST UPDATE: {NOW.date()} - {NOW.time().strftime('%H:%M:%S')}"
-        st.sidebar.write(last_update)
-        update_help = "Get up to date data."
-        update_button = st.sidebar.button("Update", help=update_help)
-        if update_button:
-            last_update = last_update
+    last_update = f"LAST UPDATE: {NOW.date()} - {NOW.time().strftime('%H:%M:%S')}"
+    st.sidebar.write(last_update)
+    update_help = "Get up to date data."
+    update_button = st.sidebar.button("Update", help=update_help)
+    #if update_button:
+    last_update = last_update
 
-            # Update data from mongo db
-            mongo_uri = "mongodb://localhost:27017/"
-            db = 'openweather_db'
-            collection = 'openweather'
-            path=f"{DATA_DIR}/landing"
-            openweather_mdb_to_json(mongo_uri=mongo_uri, db=db, collection=collection, path_to_save=path)
+    # Update data from mongo db
+    mongo_uri = "mongodb://localhost:27017/"
+    db = 'openweather_db'
+    collection = 'openweather'
+    path=f"{DATA_DIR}/landing"
+    openweather_mdb_to_json(mongo_uri=mongo_uri, db=db, collection=collection, path_to_save=path)
 
-            # RUN CLEANING - PYSPARK
-            config = openFile(f"{PROJECT_DIR}/conf/spark_session_config.json")
+    # RUN CLEANING - PYSPARK
+    config = openFile(f"{PROJECT_DIR}/conf/spark_session_config.json")
 
-            # start sparksession
-            spark = sparkStart(config)
+    # start sparksession
+    spark = sparkStart(config)
 
-            # get most recent json file from landing layer
-            last_json_filepath = [*os.walk(f"{MAIN_DIR}/data/data_lake/landing")][0][-1][-1]
-            filepath = f"{MAIN_DIR}/data/data_lake/landing/{last_json_filepath}"
+    # get most recent json file from landing layer
+    last_json_filepath = [*os.walk(f"{MAIN_DIR}/data/data_lake/landing")][0][-1][-1]
+    filepath = f"{MAIN_DIR}/data/data_lake/landing/{last_json_filepath}"
 
-            # Clean json - save as parquet.
-            df = read_json(spark, main_schema, filepath)
-            df = clean_id(df)
-            df = city_names(df)
-            df = replace_country(df)
-            df = kelvin_to_fahreheint(df, "temp")
-            df = kelvin_to_celcius(df, "temp")
-            df = extract_date(df)
-            save_as_parquet(df)
+    # Clean json - save as parquet.
+    df = read_json(spark, main_schema, filepath)
+    df = clean_id(df)
+    df = city_names(df)
+    df = replace_country(df)
+    df = kelvin_to_fahreheint(df, "temp")
+    df = kelvin_to_celcius(df, "temp")
+    df = extract_date(df)
+    save_as_parquet(df)
 
-            df = df.toPandas()
-            df = df.drop(columns=['temp', 'max_temp', 'min_temp', 'feels_like', 'id', 'city_id'])
-            df = df[['created_at', 'hour', 'country', 'city', 'lat', 'lon', 'temp_F', 'temp_C', 'humidity']]
+    df = df.toPandas()
+    df = df.drop(columns=['temp', 'max_temp', 'min_temp', 'feels_like', 'id', 'city_id'])
+    df = df[['created_at', 'hour', 'country', 'city', 'lat', 'lon', 'temp_F', 'temp_C', 'humidity']]
 
-            block1 = st.container()
-            block2 = st.container()
+    block1 = st.container()
+    block2 = st.container()
 
-            with block1:
-                data = up_to_date_by_city(df)
-                st.header("Weather Table")
-                st.write(data)
+    with block1:
+        data = up_to_date_by_city(df)
+        st.header("Weather Table")
+        st.write(data)
 
-            with block2:
-                st.header("Graph and Map")
-                col1, col2 = st.columns(2)
-                with col1:
-                    st.write(df.head())
+    with block2:
+        st.header("Graph and Map")
+        col1, col2 = st.columns(2)
+        with col1:
+            dfg = groupby_date_city(df)
+            graph_help = "Select the city to display the chart."
+            city = st.selectbox(label="City", options=dfg["city"].unique(), help=graph_help)
+            st.pyplot(line_plot_by_day(dfg, city=city))
 
-                with col2:
-                    st.map(df)
+        with col2:
+            st.line_chart(line_plot_by_day(dfg, city=city))
+            #st.map(df)
 
 
